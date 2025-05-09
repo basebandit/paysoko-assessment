91530898e94e949a862621af5dfa32e153c4bc3f
evanson <evansonmwangi83@gmail.com>
2025-05-09 15:37:47 +0300

    refactor remote backend config

--- infrastructure/terraform/provider.tf
+++ infrastructure/terraform/provider.tf
@@ -5,31 +5,31 @@ terraform {
       version = "~> 2.0"
     }
   }
-  backend "s3" {
-    endpoint                    = "fra1.digitaloceanspaces.com"
-    key                         = "terraform.tfstate"
-    bucket                      = "paysoko-assessment"
-    region                      = "eu-central-1"
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-  }
-
   # backend "s3" {
-  #   endpoints = {
-  #     s3 = "https://fra1.digitaloceanspaces.com.digitaloceanspaces.com"
-  #   }
-
-  #   bucket = "terraform.tfstate"
-  #   key    = "terraform.tfstate"
-
-  #   # Deactivate a few AWS-specific checks
-  #   skip_credentials_validation = true
-  #   skip_requesting_account_id  = true
-  #   skip_metadata_api_check     = true
-  #   skip_region_validation      = true
-  #   skip_s3_checksum            = true
+  #   endpoint                    = "fra1.digitaloceanspaces.com"
+  #   key                         = "terraform.tfstate"
+  #   bucket                      = "paysoko-assessment"
   #   region                      = "eu-central-1"
+  #   skip_credentials_validation = true
+  #   skip_metadata_api_check     = true
   # }
+
+  backend "s3" {
+    endpoints = {
+      s3 = "https://fra1.digitaloceanspaces.com"
+    }
+
+    bucket = "paysoko-assessment"
+    key    = "terraform.tfstate"
+
+    # Deactivate a few AWS-specific checks
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    skip_s3_checksum            = true
+    region                      = "eu-central-1"
+  }
 }
 
 provider "digitalocean" {
